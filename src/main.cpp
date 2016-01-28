42fd5037624cb6188f537e250efeb49be8d2ae6e
AvatarSD <sd1995@ukr.net>
2016-01-29 01:34:57 +0200

    first prepare

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include "mainwindow.h"
-#include <QApplication>
 #include "basestationiface.h"
 #include "basestationcore.h"
 #include "sonarsroutine.h"
@@ -60,27 +58,27 @@ int main(int argc, char *argv[])
     if(cmdOptionExists(argv, argv+argc, "-G"))
     {
 
-         QApplication a(argc, argv);
-
-        JoystickAdv * joy = new JoystickAdv();
-        MainWindow w(joy);
-        BaseStationIface * CopterIface = new BaseStationIface();
-        BaseStationCore * Connector = new BaseStationCore();
-
-//        Сделть наследованием!!
-        QObject::connect(CopterIface, SIGNAL(takeCmd(char*,char*,unsigned char,uint)), Connector, SLOT(inCmd(char*,char*,unsigned char,uint)));
-        QObject::connect(Connector, SIGNAL(outCmd(char*,char*,unsigned char,uint)),CopterIface, SLOT(giveCmd(char*,char*,unsigned char,uint)));
-
-        QObject::connect(joy, SIGNAL(dataRsv(JoyData*)), Connector, SLOT(inJoyData(JoyData*)));
-        QObject::connect(Connector, SIGNAL(outMetrik(mw_metric*)), &w, SLOT(metriIn(mw_metric *)));
-
-//        Убрать!
-        QObject::connect(&w, SIGNAL(Connect(int*,QString,int)),CopterIface, SLOT(Connect(int*,QString,int)));
-        QObject::connect(&w, SIGNAL(DisconnSig()),CopterIface,SLOT(Disconnect()));
-
-         w.show();
-
-         return a.exec();
+//         QApplication a(argc, argv);
+//
+//        JoystickAdv * joy = new JoystickAdv();
+//        MainWindow w(joy);
+//        BaseStationIface * CopterIface = new BaseStationIface();
+//        BaseStationCore * Connector = new BaseStationCore();
+//
+////        Сделть наследованием!!
+//        QObject::connect(CopterIface, SIGNAL(takeCmd(char*,char*,unsigned char,uint)), Connector, SLOT(inCmd(char*,char*,unsigned char,uint)));
+//        QObject::connect(Connector, SIGNAL(outCmd(char*,char*,unsigned char,uint)),CopterIface, SLOT(giveCmd(char*,char*,unsigned char,uint)));
+//
+//        QObject::connect(joy, SIGNAL(dataRsv(JoyData*)), Connector, SLOT(inJoyData(JoyData*)));
+//        QObject::connect(Connector, SIGNAL(outMetrik(mw_metric*)), &w, SLOT(metriIn(mw_metric *)));
+//
+////        Убрать!
+//        QObject::connect(&w, SIGNAL(Connect(int*,QString,int)),CopterIface, SLOT(Connect(int*,QString,int)));
+//        QObject::connect(&w, SIGNAL(DisconnSig()),CopterIface,SLOT(Disconnect()));
+//
+//         w.show();
+//
+//         return a.exec();
     }
 
     else if(cmdOptionExists(argv, argv+argc, "-S"))
